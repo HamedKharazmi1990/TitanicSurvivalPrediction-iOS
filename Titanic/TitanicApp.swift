7f03339701651667e660d5f8e1a77b8eef1a36e2
hamed kharazmi <hamed.kharazmi@bandpey.com>
2026-01-16 13:33:58 +0100

    Initial Commit

---
+++ Titanic/TitanicApp.swift
@@ -0,0 +1,17 @@
+//
+//  TitanicApp.swift
+//  Titanic
+//
+//  Created by Hamed Kharazmi on 16.01.26.
+//
+
+import SwiftUI
+
+@main
+struct TitanicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
